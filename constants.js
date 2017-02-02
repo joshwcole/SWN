8bc3b732c7bfdac5c5392f8bdae58e69ec60f02a
= <=>
2017-02-02 16:55:02 +0000

    Made a constants.js file to store the constants so generator.js is cleaner.

---
+++ constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+  atmosphere: ["Corrosive", "Inert gas", "Airless", "Breathable", "Breathable", "Breathable", "Breathable", "Breathable", "Thick atmosphere", "Toxic", "Corrosive and invasive"],
+  temperature: ["Frozen", "Cold", "Chill", "Chill", "Temperate", "Temperate", "Temperate", "Warmish", "Warmish", "Warm", "Burning"],
+  biosphere: ["Biosphere remnants", "Microbal Bio", "No Bio", "No Bio", "Human-micible bio", "Human-micible bio", "Human-micible bio", "Immicible bio", "Immicible bio", "Hybrid bio", "Engineered Bio"],
+  population: ["Failed colony", "Outpost", "Tens of Thousands", "Tens of Thousands", "Hundreds of Thousands", "Hundreds of Thousands", "Hundreds of Thousands", "Millions", "Millions", "Billions", "Aliens"],
+  techLevel: ["TL 0", "TL 1", "TL 2", "TL 3", "TL 3", "TL 4", "TL 4", "TL 4", "TL 4", "TL 4 with pre-tech or specialities", "TL 5"],
+  worldTags: [
+    ["Abandoned Colony", "Alien Ruins", "Altered Humanity", "Area 51", "Badlands World", "Bubble Cities", "Civil War", "Cold War", "Colonized Population", "Desert World"],
+    ["Eugenic Cult", "Exchange Consulate", "Feral World", "Flying Cities", "Forbidden Tech", "Freak Geology", "Freak Weather", "Friendly Foe", "Gold Rush", "Hatred"],
+    ["Heavy Industry", "Heavy Mining", "Hostile Biosphere", "Hostile Space", "Local Speciality", "Local Tech", "Major Scrapyard", "Minimal Contact", "Misandry/Misogyny", "Oceanic World"],
+    ["Out of Contact", "Outpost World", "Perimeter Agency", "Pilmgrimage Site", "Police State", "Preceptor Archive", "Pretech Cultists", "Primitive Aliens", "Psionics Fear", "Psionics Worship"],
+    ["Psionics Academy", "Quarantined World", "Radioactive World", "Regional Hegemon", "Restrictive Laws", "Rigid Culture", "Seagoing Cities", "Sealed Menace", "Sectarians", "Seismic Instability"],
+    ["Secret Masters", "Theocracy", "Tomb World", "Trade Hub", "Tyranny", "Unbraked AI", "Warlords", "Xenophiles", "Xenophobes", "Zombies"]
+  ]
+};
